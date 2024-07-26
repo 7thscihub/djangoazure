edfe11733dacb382f9fc9ce5b01171a97e8863d4
7thscihub <7thscihub@gmail.com>
2024-07-26 10:07:33 +0300

    updated manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deployment.settings')
+    settings_module = 'deployment.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'deployment.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
